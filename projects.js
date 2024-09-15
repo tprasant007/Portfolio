6776780e271e7e473c514e2eb541318093f40958
Prashant Thapa <tprasant10@gmail.com>
2024-09-15 16:08:26 +0100

    add new skills and project

--- projects.js
+++ projects.js
@@ -23,48 +23,60 @@ export const skillsArr = [
     name: "Github",
     src: "./images/icons/github.svg",
   },
+  {
+    name: "NodeJS",
+    src: "./images/icons/node.svg",
+  },
+  {
+    name: "ExpressJS",
+    src: "./images/icons/express-js.svg",
+  },
+  {
+    name: "MongoDB",
+    src: "./images/icons/mongodb.svg",
+  },
 ];
 
 export const projectsArr = [
   {
+    name: "Workout-Buddy",
+    image_src: "./images/Workout-buddy.png",
+    live_demo: "https://tprasant007-wb.netlify.app/",
+    github_repo: "https://github.com/tprasant007/workout-buddy",
+  },
+  {
     name: "Jammming",
     image_src: "./images/Jammming.png",
-    id: 1,
     live_demo: "https://prashant-jammming.netlify.app/",
     github_repo: "https://github.com/tprasant007/Jammming",
   },
   {
     name: "React Calculator",
     image_src: "./images/react-calculator.png",
-    id: 2,
     live_demo: "https://tprasant007-calculator.netlify.app/",
     github_repo: "https://github.com/tprasant007/Calculator",
   },
   {
     name: "Movie Search",
     image_src: "./images/Movie-watchlist.png",
-    id: 3,
     live_demo: "https://tprasant007.github.io/Movie-Watchlist/",
     github_repo: "https://github.com/tprasant007/Movie-Watchlist",
   },
   {
     name: "Restaurant Order App",
     image_src: "./images/restaurant-app.png",
-    id: 4,
     live_demo: "https://tprasant007-restaurant-app.netlify.app/",
     github_repo: "https://github.com/tprasant007/Restaurant-order-app",
   },
   {
     name: "Pokemon Search",
     image_src: "./images/pokemon-search.png",
-    id: 5,
     live_demo: "https://tprasant007-pokemon-search.netlify.app/",
     github_repo: "https://github.com/tprasant007/Pokemon-Search-App",
   },
   {
     name: "Random Quote Generator",
     image_src: "./images/random-quote.png",
-    id: 6,
     live_demo: "https://tprasant007-random-quotes-generator.netlify.app/",
     github_repo: "https://github.com/tprasant007/random-quotes",
   },
